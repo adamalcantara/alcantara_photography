e364db30d41f24acfe59ee5626e485837102f412
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-12 12:43:03 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
-import {BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./Components/Nav/Nav"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./Components/Nav/Nav";
 
 //Import all the pages
-import Home from "./Pages/Home/Home"
-import About from "./Pages/About/About"
-import Portraits from "./Pages/Portraits/Portraits"
-import Headshots from "./Pages/Headshots/Headshots"
-import Weddings from "./Pages/Weddings/Weddings"
-import Contact from "./Pages/Contact/Contact"
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Portraits from "./Pages/Portraits/Portraits";
+import Headshots from "./Pages/Headshots/Headshots";
+import Weddings from "./Pages/Weddings/Weddings";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="pageContent">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portraits" component={Portraits} />
+          <Route exact path="/headshots" component={Headshots} />
+          <Route exact path="/weddings" component={Weddings} />
+          <Route exact path="/contact" component={Contact} />
+        </div>
+      </div>
+    </Router>
+
   );
 }
 
