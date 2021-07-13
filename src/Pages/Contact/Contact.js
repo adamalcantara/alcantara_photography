51d9833cd1eb3599dde7dd426a3c9bc995632c62
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-13 16:05:47 -0400

    Added contact form

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Form from "../../Components/ContactForm/ContactForm"
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                Welcome to the contact page
+                <Form />
             </div>
         );
     }
